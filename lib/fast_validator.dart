e41985c31ec227a6ac94d6adf63682ef035ee465
christo pb <christo@ateamsoftsolutions.com>
2024-01-20 16:46:34 +0530

    Update: exported files in model

--- lib/fast_validator.dart
+++ lib/fast_validator.dart
@@ -2,3 +2,6 @@ library fast_cached_network_image;
 
 export 'src/built_in_validators.dart';
 export 'src/validator.dart';
+export 'src/models/fast_validator.dart';
+export 'src/models/result_type.dart';
+export 'src/models/validation_result.dart';

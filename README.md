8d0bb42d8fb8088c7882b0935336f3530855f1fb
christo pb <christo@ateamsoftsolutions.com>
2024-02-02 23:45:26 +0530

    Update: Added example

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 # Fast Validator
 
 A flutter package to validate text fields, date fields or any data fastly. The package contains a list of built in commonly used validators. You can also add custom validators for your needs.
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://pub.dev/packages/fast_validator)
+[![pub](https://img.shields.io/pub/v/fast_cached_network_image)](https://pub.dev/packages/fast_validator)
+[![dart](https://img.shields.io/badge/dart-pure%20dart-success)](https://pub.dev/packages/fast_validator)
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,77 +1,75 @@
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatefulWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   late TextEditingController textController;
-//   String? error;
-//
-//   @override
-//   void initState() {
-//     textController = TextEditingController();
-//     super.initState();
-//   }
-//
-//   @override
-//   void dispose() {
-//     textController.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               const Text('User email *'),
-//               SizedBox(
-//                 width: 200,
-//                 height: 40,
-//                 child: TextField(
-//                   controller: textController,
-//                   decoration: const InputDecoration(
-//                     contentPadding: EdgeInsets.symmetric(horizontal: 6, vertical: 0),
-//                     border: OutlineInputBorder(borderSide: BorderSide(width: 3, color: Colors.green)),
-//                   ),
-//                 ),
-//               ),
-//               Text(error ?? '', style: const TextStyle(color: Colors.red)),
-//             ],
-//           ),
-//         ),
-//         floatingActionButton: FloatingActionButton(onPressed: _validate, child: const Text('Validate')),
-//       ),
-//     );
-//   }
-//
-//   void _validate() {
-//     /// Reset the error
-//     setState(() => error = null);
-//
-//     String text = textController.text.trim();
-//
-//     ValidationResult result = FastValidator.validate(text,
-//         fieldName: 'Email',
-//         validators: [FastRequiredValidation(), FastEmailValidation(customMessage: 'Invalid email')]);
-//
-//     debugPrint('Result: ${result.result}');
-//     debugPrint('Valid: ${result.valid}');
-//     debugPrint('Type: ${result.type}');
-//     debugPrint('ErrorMsg: ${result.errorMessage}');
-//
-//     if (!result.valid) setState(() => error = result.errorMessage);
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:fast_validator/fast_validator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TextEditingController textController;
+  String? error;
+
+  @override
+  void initState() {
+    textController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('User email *'),
+              SizedBox(
+                width: 200,
+                height: 40,
+                child: TextField(
+                  controller: textController,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 6, vertical: 0),
+                    border: OutlineInputBorder(borderSide: BorderSide(width: 3, color: Colors.green)),
+                  ),
+                ),
+              ),
+              Text(error ?? '', style: const TextStyle(color: Colors.red)),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: _validate, child: const Text('Validate')),
+      ),
+    );
+  }
+
+  void _validate() {
+    String text = 'user@gmail..com';
+
+    ValidationResult result = FastValidator.validate(text,
+        fieldName: 'Email',
+        validators: [FastRequiredValidation(), FastEmailValidation(customMessage: 'Invalid email')]);
+
+    debugPrint('Result: ${result.result}');
+    debugPrint('Valid: ${result.valid}');
+    debugPrint('Type: ${result.type}');
+    debugPrint('ErrorMsg: ${result.errorMessage}');
+
+    if (!result.valid) setState(() => error = result.errorMessage);
+  }
+}
